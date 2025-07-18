41aadf39bcf09fecb0ca659f390912245913b349
Mazaxaka_sf <arty_h41@mail.ru>
2025-07-18 19:17:25 +0300

    main py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Optional
+from datetime import date
+from pydantic import BaseModel
+
 # from .database import engine
 # from . import models
 # from .routers import items
@@ -7,8 +11,40 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 # app.include_router(items.router)
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FastAPI!"}
+
+
+@app.get('/items')
+def get_items(
+        author: str,
+        date_from: date,
+        date_to: date,
+        view: Optional[int] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+):
+    "Optional - не обязательный параметр"
+    "Query - Ограничение от 1 до 5 "
+    return f"item: "
+
+class SCategory(BaseModel):
+    cat_id: int
+    name: str
+    description: str
+
+@app.post("/category")
+def add_category(category: SCategory):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", reload=True)  # hello - name file
+    # reload перезапустить сервер если садержимое изменится
